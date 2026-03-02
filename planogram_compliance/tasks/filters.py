cb5e09624068c0925c994ac38232c836b56d3c75
Shivam Jain <59995800+shivamjain164@users.noreply.github.com>
2026-03-02 16:26:53 +0530

    Delete planogram_compliance directory

--- planogram_compliance/tasks/filters.py
+++
@@ -1,7 +0,0 @@
-import django_filters
-from . import models
-
-class TaskFilter(django_filters.FilterSet):
-    class Meta:
-        model = models.Tasks
-        fields = ['site', 'department', 'category', 'status', "planogram", "deadline", "user"]
